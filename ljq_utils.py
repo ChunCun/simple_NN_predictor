71592364c66cbbc98c87dba1a83bc03c344e4d83
Chunhui Yang <alexchun31@163.com>
2018-11-30 00:52:24 -0600

    update

--- ljq_utils.py
+++ ljq_utils.py
@@ -2,13 +2,13 @@ import numpy as np
 
 def load_data(data_path):
     input = np.loadtxt(data_path)
-    data = np.reshape(input,(1,1000))
+    data = np.reshape(input,(1,96))
 
     return data
 
 def load_label(label_path):
     input = np.loadtxt(label_path)
-    data = data = np.reshape(input,(1,250))
+    data = data = np.reshape(input,(1,24))
 
     return data
 
